456227e244b2939f67518fe4e8a5a7dd9e79d619
kll810 <418904718@qq.com>
2018-01-03 10:54:31 +0800

    更新完善

--- page/js/plugins.js
+++ page/js/plugins.js
@@ -219,33 +219,34 @@ $(document).ready(function() {
             $(this).toggleClass("active");
         })
         //添加购物车
-    $('.js-add-cart').click(function(e) {
-        e.stopPropagation();
-        var number = Number($(this).parents("tr").find(".priceInput").val());
-        var productimg = $(this).parents("tr").find(".proimg").children("img"),
-            imgsrc = productimg.attr("src"),
-            x = productimg.offset().left + 30,
-            y = productimg.offset().top - 10,
-            X = $(".js-shopping-cart").offset().left,
-            Y = $(".js-shopping-cart").offset().top;
-        if ($('#flydiv').length <= 0) {
-            $('body').append('<div id="flydiv"><img src="' + imgsrc + '" width="50" height="50" /></div');
-        };
-        var $obj = $('#flydiv');
-        if (!$obj.is(':animated')) {
-            $obj.css({ 'left': x, 'top': y }).animate({ 'left': X, 'top': Y - 80 }, 500, function() {
-                $obj.stop(false, false).animate({ 'top': Y - 20, 'opacity': 0 }, 500, function() {
-                    $obj.fadeOut(300, function() {
-                        $obj.remove();
-                        var num = Number($(".cartnums").text());
-                        $(".cartnums").text(num + number);
-                        $(".cartnums").show();
-                    });
+    $('.js-add-cart').click(addCart)
+});
+function addCart(e) {
+    e.stopPropagation();
+    var number = Number($(this).parents(".js-line-parents").find(".priceInput").val());
+    var productimg = $(this).parents(".js-line-parents").find(".js-img").children("img"),
+        imgsrc = productimg.attr("src"),
+        x = productimg.offset().left + 30,
+        y = productimg.offset().top - 10,
+        X = $(".js-shopping-cart").offset().left,
+        Y = $(".js-shopping-cart").offset().top;
+    if ($('#flydiv').length <= 0&&number!=0) {
+        $('body').append('<div id="flydiv"><img src="' + imgsrc + '" width="50" height="50" /></div');
+    };
+    var $obj = $('#flydiv');
+    if (!$obj.is(':animated')) {
+        $obj.css({ 'left': x, 'top': y }).animate({ 'left': X, 'top': Y - 80 }, 500, function() {
+            $obj.stop(false, false).animate({ 'top': Y - 20, 'opacity': 0 }, 500, function() {
+                $obj.fadeOut(300, function() {
+                    $obj.remove();
+                    var num = Number($(".cartnums").text());
+                    $(".cartnums").text(num + number);
+                    $(".cartnums").show();
                 });
             });
-        };
-    })
-});
+        });
+    };
+}
 //购物车总价计算-shopping
 $(document).ready(function() {
     var _total = 0;
@@ -570,3 +571,34 @@ $(document).ready(function() {
     };
     $('[data-hover="dropdown"]').dropdownHover();
 })(jQuery, this);
+// 设置为主页 
+function SetHome(obj, vrl) {
+    try {
+        obj.style.behavior = 'url(#default#homepage)';
+        obj.setHomePage(vrl);
+    } catch (e) {
+        if (window.netscape) {
+            try {
+                netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+            } catch (e) {
+                alert("此操作被浏览器拒绝！\n请在浏览器地址栏输入“about:config”并回车\n然后将 [signed.applets.codebase_principal_support]的值设置为'true',双击即可。");
+            }
+            var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch);
+            prefs.setCharPref('browser.startup.homepage', vrl);
+        } else {
+            alert("您的浏览器不支持，请按照下面步骤操作：1.打开浏览器设置。2.点击设置网页。3.输入：" + vrl + "点击确定。");
+        }
+    }
+}
+// 加入收藏 兼容360和IE6 
+function shoucang(sTitle, sURL) {
+    try {
+        window.external.addFavorite(sURL, sTitle);
+    } catch (e) {
+        try {
+            window.sidebar.addPanel(sTitle, sURL, "");
+        } catch (e) {
+            alert("加入收藏失败，请使用Ctrl+D进行添加");
+        }
+    }
+}
